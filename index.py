0faf57a55400bcb0ee27d360633115e9ea3224cd
Keyyard <84187238+Keyyard@users.noreply.github.com>
2024-09-04 06:26:14 +0700

    Update index.py

--- index.py
+++ index.py
@@ -11,16 +11,6 @@ output_folder = 'output'
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-#loop through all wav files in input folder
-#for wav_file in glob.glob(os.path.join(input_folder, '*.wav')):
-    #read wav file
-#  data, samplerate = sf.read(wav_file)
-    #get file name
-#   file_name = os.path.splitext(os.path.basename(wav_file))[0]
-    #write ogg file
-#    sf.write(os.path.join(output_folder, file_name + '.ogg'), data, samplerate)
-
-#loop through all wav files in input folders, there maybe even folders inside folders and create the same folder structures then output the oggs in correct places
 for root, dirs, files in os.walk(input_folder):
     for file in files:
         if file.endswith(".wav"):
